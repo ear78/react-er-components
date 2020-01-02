a2ee064e415dfe69bb0e865ae8d6a16a1fa00243
elliot richardson <ear78@aol.com>
2020-01-01 18:05:43 -0800

    added images for demo pods, mapped out templates and need to create data for individual pod [wip]

--- src/HoverPods/HoverPods.js
+++ src/HoverPods/HoverPods.js
@@ -1,19 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './HoverPods.module.scss'
 
 import building from '../assets/img/pinkBuilding.jpg'
 
 function hoverPods(props) {
-  return (
-    <section>
-      <div style={{ backgroundImage: `url(${building})`}} className={styles.Pod}>
+let podData = [
+  { podBgImage: building,
+    podBgColor: '#c50043ad',
+    podTextColor: '#fff',
+    podText: 'Hello'
+  },
+  { podBgImage: building,
+    podBgColor: '#c50043ad',
+    podTextColor: '#fff',
+    podText: 'Hello'
+  },
+  { podBgImage: building,
+    podBgColor: '#c50043ad',
+    podTextColor: '#fff',
+    podText: 'Hello'
+  },
+  { podBgImage: building,
+    podBgColor: '#c50043ad',
+    podTextColor: '#fff',
+    podText: 'Hello'
+  },
+]
 
-        <div className={styles.HoverColor}>
-          <p className={styles.HoverText}>Hello</p>
-        </div>
-      </div>
+let hoverPod = podData.map(pod => {
+  return <div
+    style={{ backgroundImage: `url(${pod.podBgImage})`}}
+    className={styles.Pod}>
+    <div
+      style={{ backgroundColor: `${pod.podBgColor}`}} className={styles.HoverColor}>
+      <p
+        style={{ color: `${pod.podTextColor}`}} className={styles.HoverText}>{pod.podText}</p>
+    </div>
+  </div>
+})
+  return (
+    <section className={styles.HoverPodsContainer}>
+      { hoverPod }
     </section>
   )
+}
+
+hoverPods.propTypes = {
+  podBgColor: PropTypes.string,
+  podTextColor: PropTypes.string,
+  podText: PropTypes.string
 }
 
 export default hoverPods
