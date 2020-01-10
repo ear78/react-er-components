bfca02b3175277b309f0f09a864d9b9b077264f1
elliot richardson <ear78@aol.com>
2020-01-09 23:03:30 -0800

    updated team building component, and moved data to data.js

--- src/TeamBuilding/TeamBuilding.js
+++ src/TeamBuilding/TeamBuilding.js
@@ -2,34 +2,15 @@ import React from 'react'
 import styles from './TeamBuilding.module.scss'
 
 import logo from '../assets/img/er-logo.svg'
-import image from '../assets/img/jack-reacher.jpg'
+import { portfolios } from '../assets/js/data'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function teamBuilding() {
-  let portfolios = [
-    {
-      name: 'Jack Reacher',
-      jobTitle: 'Professional Hitman',
-    },
-    {
-      name: 'Jack Reacher',
-      jobTitle: 'Professional Hitman',
-    },
-    {
-      name: 'Jack Reacher',
-      jobTitle: 'Professional Hitman',
-    },
-    {
-      name: 'Jack Reacher',
-      jobTitle: 'Professional Hitman',
-    },
-  ]
-
-  let portfolio = portfolios.map(member => {
-    return <div className={styles.Portfolio}>
-                  <img className={styles.Image} src={image} alt="Jack Reacher"/>
-                  <h3 className={styles.Name}>Jack Reacher</h3>
-                  <p className={styles.JobTitle}>Professional Killer</p>
+function teamBuilding(props) {
+  let portfolio = portfolios.map((member, i) => {
+    return <div key={i} className={styles.Member}>
+                  <img className={styles.Image} src={member.image} alt={member.name}/>
+                  <h3 className={styles.Name}>{member.name}</h3>
+                  <p className={styles.JobTitle}>{member.jobTitle}</p>
                   <img className={styles.CompanyLogo} src={logo} />
                   <div className={styles.icons}>
                     <FontAwesomeIcon icon={['fab', 'facebook-square']}/>
@@ -39,8 +20,10 @@ function teamBuilding() {
                 </div>
   })
   return (
-    <div id={styles.TeamBuildingContainer}>
-      {portfolio}
+    <div>
+      <div id={styles.TeamBuildingContainer}>
+          {portfolio}
+      </div>
     </div>
   )
 }
--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -2,6 +2,16 @@ import building from '../img/pinkBuilding.jpg'
 import snowGhosts from '../img/snowghosts.jpg'
 import greyBuilding from '../img/building-resize.jpg'
 import about from '../img/ABOUT.jpg'
+import portfolio1 from '../img/jack-reacher.jpg'
+import portfolio2 from '../img/dave-m.jpg'
+import portfolio3 from '../img/ethan-l.jpg'
+import portfolio4 from '../img/kim-m.jpg'
+import portfolio5 from '../img/doug-l.jpg'
+import portfolio6 from '../img/paran-j.jpg'
+import portfolio7 from '../img/kevin.jpg'
+import portfolio8 from '../img/krish.jpg'
+import portfolio9 from '../img/jeremy.jpg'
+import portfolio10 from '../img/robert.jpg'
 
 export const podData = [
   { podBgImage: building,
@@ -24,4 +34,57 @@ export const podData = [
     podTextColor: '#fff',
     podText: 'Hover Pod'
   },
+]
+
+export const  portfolios = [
+  {
+    image: portfolio1,
+    name: 'Jack Reacher',
+    jobTitle: 'Professional Hitman',
+  },
+  {
+    image: portfolio2,
+    name: 'David Mattias',
+    jobTitle: 'General Manager',
+  },
+  {
+    image: portfolio3,
+    name: 'Elias Leonard',
+    jobTitle: 'Head of Technology',
+  },
+  {
+    image: portfolio4,
+    name: 'Megan Kimberly',
+    jobTitle: 'Art Director',
+  },
+  {
+    image: portfolio5,
+    name: 'Johnny Reb',
+    jobTitle: 'Computerist',
+  },
+  {
+    image: portfolio6,
+    name: 'Paran John',
+    jobTitle: 'Senior Developer',
+  },
+  {
+    image: portfolio9,
+    name: 'Elias Leonard',
+    jobTitle: 'Head of Technology',
+  },
+  {
+    image: portfolio10,
+    name: 'Megan Kimberly',
+    jobTitle: 'Art Director',
+  },
+  {
+    image: portfolio7,
+    name: 'David Mattias',
+    jobTitle: 'General Manager',
+  },
+  {
+    image: portfolio8,
+    name: 'Elias Leonard',
+    jobTitle: 'Head of Technology',
+  }
 ]
