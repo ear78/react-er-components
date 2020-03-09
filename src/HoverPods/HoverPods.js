26c7290b610ea74eaa1927a40d79ed7dc92cae6e
elliot richardson <ear78@aol.com>
2020-03-08 22:53:48 -0700

    added animation adjustment for hoverpods

--- src/HoverPods/HoverPods.js
+++ src/HoverPods/HoverPods.js
@@ -4,36 +4,55 @@ import PropTypes from 'prop-types'
 import styles from './HoverPods.module.scss'
 import PageTitle from '../components/PageTitle/PageTitle'
 
-function hoverPods( props ) {
+class hoverPods extends React.Component {
+  state = {
+    hoverPodsMounted: false
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((state) => {
+        return {hoverPodsMounted: !state.hoverPodsMounted}
+      })
+    }, 900)
 
-  let hoverPod = props.podData.map( ( pod, index ) => {
-    return <div
-    key={index}
-    style={{ backgroundImage: `url(${pod.podBgImage})`, transitionDelay: `${ index * .1 }s` }}
-    className={styles.Pod}>
-    <div style={{ backgroundColor: `${pod.podBgColor}`}} className={styles.HoverColor}>
-      <p style={{ color: `${pod.podTextColor}`}} className={styles.HoverText}>{pod.podText}</p>
-    </div>
-  </div>
-  } )
-  return (
-    <section id={styles.HoverPodsPageContainer}>
-      <PageTitle title="Hover Pods"/>
-      <CSSTransitionGroup component="div" className={styles.HoverPodsContainer} transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500} transitionEnter={true}
-      transitionLeave={true} transitionName={ {
-      enter: styles.enter,
-      enterActive: styles.enterActive,
-      leave: styles.leave,
-      leaveActive: styles.leaveActive,
-      appear: styles.appear,
-      appearActive: styles.appearActive
-  } }>
+  }
+  render() {
+    let hoverPod
+    if(this.state.hoverPodsMounted) {
+      hoverPod = this.props.podData.map( ( pod, index ) => {
+        return <div
+        key={index}
+        style={{ backgroundImage: `url(${pod.podBgImage})`, transitionDelay: `${ index * .1 }s` }}
+        className={styles.Pod}>
+        <div style={{ backgroundColor: `${pod.podBgColor}`}} className={styles.HoverColor}>
+          <p style={{ color: `${pod.podTextColor}`}} className={styles.HoverText}>{pod.podText}</p>
+        </div>
+      </div>
+      })
+    }
+    else {
+      hoverPod = null
+    }
 
-  {hoverPod}
-    </CSSTransitionGroup>
-    </section>
+    return (
+      <section id={styles.HoverPodsPageContainer}>
+        <PageTitle title="Hover Pods"/>
+        <CSSTransitionGroup component="div" className={styles.HoverPodsContainer} transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500} transitionEnter={true}
+        transitionLeave={true} transitionName={ {
+        enter: styles.enter,
+        enterActive: styles.enterActive,
+        leave: styles.leave,
+        leaveActive: styles.leaveActive,
+        appear: styles.appear,
+        appearActive: styles.appearActive
+    } }>
 
-  )
+        {hoverPod}
+      </CSSTransitionGroup>
+      </section>
+
+    )
+  }
 }
 
 hoverPods.propTypes = {
