8553434b8cbf104b03a92015255b06395e5c4d66
elliot richardson <ear78@aol.com>
2020-05-17 23:28:08 -0700

    updated happy dots and scss variables/mixins

--- src/views/HappyDots/HappyDotsParent.js
+++ src/views/HappyDots/HappyDotsParent.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import styles from './HappyDotsParent.module.scss'
 import PageTitle from '../../components/PageTitle/PageTitle'
+import Spacer from '../../components/Spacer/Spacer'
 import HappyDots from './HappyDots'
 import { happyData, sectionData } from '../../assets/js/data'
 
 class HappyDotsParent extends React.Component {
   render() {
     return (
-      <div>
-        <PageTitle title='HappyDots' alt={true}/>
+      <div className={styles.HappyDotsParent}>
+        <PageTitle title='HappyDots'/>
+        <Spacer height={50}/>
         <HappyDots data={happyData} sectionData={sectionData} textColor='#fff'/>
       </div>
     )
