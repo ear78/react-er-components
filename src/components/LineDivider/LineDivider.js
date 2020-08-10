4654a3d9604f628e9b1b0f6b065560a7a9dddf17
elliot richardson <ear78@aol.com>
2020-08-09 17:05:53 -0700

    Added border bottom to happyDots sections. Added line divider component

---
+++ src/components/LineDivider/LineDivider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LineDivider() {
+    let LineDividerStyle = {
+        height: '1px',
+        width: '80%',
+        backgroundColor: '#ebebeb'
+    }
+    return <div style={LineDividerStyle}></div>
+}
+
+export default LineDivider
